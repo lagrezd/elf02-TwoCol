1b388355486b673ac1e6c875e74e6e079a1ea03f
ChrisB <kontakt@elf02.de>
2014-06-22 14:55:53 +0200

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp         = require('gulp'),
     cssmin       = require('gulp-cssmin'),
     rename       = require('gulp-rename'),
     concat       = require('gulp-concat'),
+    jshint       = require('gulp-jshint'),
     uglify       = require('gulp-uglify'),
     notify       = require('gulp-notify'),
     watch        = require('gulp-watch'),
@@ -13,18 +14,21 @@ var gulp         = require('gulp'),
 
 gulp.task('sass', function() {
     return gulp.src('./scss/style.scss')
+
         // sass, notify
         .pipe(sass({
             errLogToConsole: false,
             onError: function(err) {
-                return notify().write(err);
+                return notify().write('sass: ' + err);
             }
         }))
         .pipe(gulp.dest('./dist'))
+
         // autoprefixer
         .pipe(rename('style.prefixed.css'))
         .pipe(autoprefixer('last 2 version', 'ie 9'))
         .pipe(gulp.dest('./dist'))
+
         // cssmin
         .pipe(rename('style.min.css'))
         .pipe(cssmin())
@@ -34,12 +38,31 @@ gulp.task('sass', function() {
 
 gulp.task('js', function() {
     gulp.src(['./js/*/*.js', './js/scripts.js'])
+
+        // jshint
+        .pipe(jshint())
+        .pipe(notify(function (file) {
+            if (file.jshint.success) return false;
+            var errors = file.jshint.results.map(function (data) {
+                if (data.error) {
+                  return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+                }
+            }).join("\n");
+            return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
+        }))
+
         // concat
         .pipe(concat('scripts.concat.js'))
         .pipe(gulp.dest('./dist'))
+
         // uglify
         .pipe(rename('scripts.min.js'))
-        .pipe(uglify())
+        .pipe(uglify({
+            preserveComments: 'some'
+        }).on('error', function(e) {
+            notify().write('uglify: ' + e.message);
+            return this.end()
+        }))
         .pipe(gulp.dest('./dist'));
 });
 
